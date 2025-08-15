d22ab5c6c0bf01eac6afcc18b5e8fc218685f079
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-08-15 06:36:22 +0530

    Added the proper tailwind config in index.css, also put the components and pages folder inside the src folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar.jsx';
 import AllApplications from "./pages/AllApplications";
 
 //Day 1 : Single page (routing comes later)
