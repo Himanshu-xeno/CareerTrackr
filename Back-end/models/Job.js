9c1dcb8d328a7e347936999b9dfd6393699595f7
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-08-26 23:19:56 +0530

    Updated the Job.js with ESM stlye for export and import to be consisitent in code

--- Back-end/models/Job.js
+++ Back-end/models/Job.js
@@ -1,42 +1,37 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-//Define the Job schema 
+// Define the Job schema
 const jobSchema = new mongoose.Schema({
-title: { 
-  type : String,
-  required : true, //job must have a title 
-},
-
-company : {
-  type : String,
-  required : true, //Company must have a name 
-},
-
-location : {
-  type : String,
-  required : true, //Location cannot be empty 
-},
-
-status : {
-  type : String,
-  enum : ["Applied", "Interview", "Offer", "Rejected"],  //Only allow these values here 
-  default : "Applied",
-},
-
-priority : {
-  type : String,
-  enum : ["High", "Medium", "Low"],
-  default : "Medium",
-},
-
-dateApplied : {
-  type : Date,
-  default : Date.now, // if not given, take current date
-},
-
+  title: {
+    type: String,
+    required: true, // job must have a title
+  },
+  company: {
+    type: String,
+    required: true, // Company must have a name
+  },
+  location: {
+    type: String,
+    required: true, // Location cannot be empty
+  },
+  status: {
+    type: String,
+    enum: ["Applied", "Interview", "Offer", "Rejected"], // Only allow these values
+    default: "Applied",
+  },
+  priority: {
+    type: String,
+    enum: ["High", "Medium", "Low"],
+    default: "Medium",
+  },
+  dateApplied: {
+    type: Date,
+    default: Date.now, // if not given, take current date
+  },
 });
 
-//Create the model 
+// Create the model
 const Job = mongoose.model("Job", jobSchema);
 
-module.exports = Job;
+// ✅ Export in ESM style
+export default Job;
