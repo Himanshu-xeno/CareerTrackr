b0955adacf8389342a10ace262220abf22c80c10
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-08-26 22:58:52 +0530

    added the POST api in jobRoutes.js

--- Back-end/routes/jobRoutes.js
+++ Back-end/routes/jobRoutes.js
@@ -7,4 +7,30 @@ router.get("/", (req,res) => {
   res.send("Jobs API is working");
 });
 
+
+//POST : Add a new job 
+router.post("/", async (req,res) => {
+  try{
+    const{title, company, location, status, priority, date} = req.body;
+
+    //Create a new Job document
+    const newJob = new Job({
+      title,
+      company,
+      location,
+      status,
+      priority,
+      date,
+    });
+
+    //save to DB 
+    const savedJob = await newJob.save();
+
+    //Send back the saved job
+    res.status(201).json(savedJob);
+  }catch(err){
+    res.status(500).json({error:err.message});
+  }
+});
+
 export default router;
