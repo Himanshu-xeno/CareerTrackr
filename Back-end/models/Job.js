bc7062e3a3542a4bd7e5ebd5195758bca9ace5b9
Himanshu Tiwari <himanshusktiwari@gmail.com>
2025-08-26 18:49:42 +0530

    Created the schema and model

--- Back-end/models/Job.js
+++ Back-end/models/Job.js
@@ -1,19 +1,42 @@
 const mongoose = require("mongoose");
 
+//Define the Job schema 
 const jobSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  company: { type: String, required: true },
-  status: {
-    type: String,
-    enum: ["Applied", "Interview", "Offer", "Rejected"],
-    default: "Applied"
-  },
-  priority: {
-    type: String,
-    enum: ["High", "Medium", "Low"],
-    default: "Medium"
-  },
-  date: { type: Date, default: Date.now }
+title: { 
+  type : String,
+  required : true, //job must have a title 
+},
+
+company : {
+  type : String,
+  required : true, //Company must have a name 
+},
+
+location : {
+  type : String,
+  required : true, //Location cannot be empty 
+},
+
+status : {
+  type : String,
+  enum : ["Applied", "Interview", "Offer", "Rejected"],  //Only allow these values here 
+  default : "Applied",
+},
+
+priority : {
+  type : String,
+  enum : ["High", "Medium", "Low"],
+  default : "Medium",
+},
+
+dateApplied : {
+  type : Date,
+  default : Date.now, // if not given, take current date
+},
+
 });
 
-module.exports = mongoose.model("Job", jobSchema);
+//Create the model 
+const Job = mongoose.model("Job", jobSchema);
+
+module.exports = Job;
